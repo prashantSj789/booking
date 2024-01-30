79b3cb99122d53876b0b3b185939d118ef53598d
prashantSj789 <singhrashant79072@gmail.com>
2024-01-30 14:49:57 +0530

    converted html to go templates

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(witetoconsole)
 	mux.Get("/",handlers.Repo.Home)
 	mux.Get("/about",handlers.Repo.About)
+	mux.Get("/generals-quarters",handlers.Repo.Generals)
+	mux.Get("/majors-suite",handlers.Repo.Majors)
+	mux.Get("/make-reservation",handlers.Repo.Reservation)
 	fileserver:= http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*",http.StripPrefix("/static",fileserver))
 	
